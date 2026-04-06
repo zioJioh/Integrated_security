f0fdecf5ab9d5dc9bca0ac0bbd42490053ff568e
gioJioh <sjgio0515@naver.com>
2026-04-06 10:43:25 +0900

    19-f

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -3,6 +3,7 @@ export function fetchApi(url: string, options?: RequestInit) {
         const headers = new Headers(options.headers || {});
         headers.set("Content-Type", "application/json");
         options.headers = headers;
+        options.credentials = "include";
     }
 
     return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options).then(
