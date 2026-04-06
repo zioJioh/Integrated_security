9f9d6531df48fe605ead38cf8897c6c31657fbe0
gioJioh <sjgio0515@naver.com>
2026-04-06 11:33:16 +0900

    22-f

--- frontend/src/app/clientLayout.tsx
+++ frontend/src/app/clientLayout.tsx
@@ -1,7 +1,21 @@
 "use client";
+import { fetchApi } from "@/lib/client";
 import Link from "next/link";
 
 export default function ClientLayouts({ children }: { children: React.ReactNode }) {
+
+    const logout = () => {
+        confirm("로그아웃 하시겠습니까?") &&
+            fetchApi("/api/v1/members/logout", {
+                method: "DELETE",
+            })
+                .then((data) => {
+                    alert(data.msg);
+                })
+                .catch((rsData) => {
+                    alert(rsData.msg);
+                });
+    };
 
     return <>
 
@@ -11,9 +25,8 @@ export default function ClientLayouts({ children }: { children: React.ReactNode 
                 <Link href="/posts">목록</Link>
                 <Link href="/member/login">로그인</Link>
                 <button className="hover:cursor-pointer"
-                    onClick={() => {
-                        console.log("로그아웃 수행");
-                    }}>로그아웃</button>
+                    onClick={logout}
+                >로그아웃</button>
             </nav>
         </header >
         <main className="flex flex-col flex-grow gap-4 justify-center items-center">
--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -1,9 +1,12 @@
 export function fetchApi(url: string, options?: RequestInit) {
+
+    options = options || {};
+    options.credentials = "include";
+
     if (options?.body) {
         const headers = new Headers(options.headers || {});
         headers.set("Content-Type", "application/json");
         options.headers = headers;
-        options.credentials = "include";
     }
 
     return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options).then(
